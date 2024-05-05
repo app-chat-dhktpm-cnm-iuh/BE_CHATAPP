e28a992ca14e9ff7b315a009775b6fed99b63dbc
hophuocduy2610 <hophuocduy261020@gmail.com>
2024-05-05 13:22:11 +0700

    fixed class attach

--- src/main/java/com/example/BEChatAppCNM/entities/Attach.java
+++ src/main/java/com/example/BEChatAppCNM/entities/Attach.java
@@ -1,5 +1,14 @@
 package com.example.BEChatAppCNM.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Attach {
     private String url;
     private String name;
